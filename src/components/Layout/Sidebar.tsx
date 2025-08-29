d483cf001b6ecc7f6a1a1988a112cb7a275971f2
Anand Pandey <ap3832576@gmail.com>
2025-08-30 00:50:08 +0530

    Update Sidebar.tsx

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -171,7 +171,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeSection, onSectionChange, onHom
             <Library className="w-6 h-6 text-white" />
           </div>
           <div>
-              <h1 className="text-xl font-bold text-gray-900">BookZone</h1>
+              <h1 className="text-xl font-bold text-gray-900">ShelfaryX</h1>
             <p className="text-sm text-gray-500">Librarian Portal</p>
           </div>
         </div>
